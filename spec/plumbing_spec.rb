43f45350059cdafd4a9dc4f59254dfea5e25d55b
Joe Atzberger <ohiocore@gmail.com>
2015-09-05 00:12:55 -0700

    Extra tests for configure around logger

---
+++ spec/plumbing_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Dor::WorkflowService do
+  before :each do
+    @logger1 = double('Logger')
+    @logger2 = double('Logger')
+    allow(Dor::WorkflowService).to receive(:default_logger).and_return(@logger1)
+  end
+  describe '#configure' do
+    it 'pulls default_logger if not specified' do
+      expect(Dor::WorkflowService).to receive(:default_logger).and_return(@logger1)
+      Dor::WorkflowService.configure('https://dortest.stanford.edu/workflow')
+    end
+    it 'accepts :logger if specified' do
+      expect(Dor::WorkflowService).not_to receive(:default_logger)
+      Dor::WorkflowService.configure('https://dortest.stanford.edu/workflow', {:logger => @logger1})
+    end
+  end
+end
