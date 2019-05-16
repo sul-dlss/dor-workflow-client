da181cdef97ea20e7b6cd4766dea9dd9650e20c6
Justin Coyne <jcoyne@justincoyne.com>
2019-05-16 09:18:50 -0500

    Refactor: Don't repeat code

--- lib/dor/workflow/client/lifecycle_routes.rb
+++ lib/dor/workflow/client/lifecycle_routes.rb
@@ -12,39 +12,21 @@ module Dor
         # Returns the Date for a requested milestone from workflow lifecycle
         # @param [String] repo repository name
         # @param [String] druid object id
-        # @param [String] milestone name of the milestone being queried for
+        # @param [String] milestone_name the name of the milestone being queried for
         # @return [Time] when the milestone was achieved.  Returns nil if the milestone does not exist
-        # @example An example lifecycle xml from the workflow service.
-        #   <lifecycle objectId="druid:ct011cv6501">
-        #     <milestone date="2010-04-27T11:34:17-0700">registered</milestone>
-        #     <milestone date="2010-04-29T10:12:51-0700">inprocess</milestone>
-        #     <milestone date="2010-06-15T16:08:58-0700">released</milestone>
-        #   </lifecycle>
-        def lifecycle(repo, druid, milestone)
-          doc = query_lifecycle(repo, druid)
-          milestone = doc.at_xpath("//lifecycle/milestone[text() = '#{milestone}']")
-          return Time.parse(milestone['date']) if milestone
-
-          nil
+        #
+        def lifecycle(repo, druid, milestone_name)
+          filter_milestone(query_lifecycle(repo, druid), milestone_name)
         end
 
         # Returns the Date for a requested milestone ONLY FROM THE ACTIVE workflow table
         # @param [String] repo repository name
         # @param [String] druid object id
-        # @param [String] milestone name of the milestone being queried for
-        # @return [Time] when the milestone was achieved.  Returns nil if the milestone does not exist
-        # @example An example lifecycle xml from the workflow service.
-        #   <lifecycle objectId="druid:ct011cv6501">
-        #     <milestone date="2010-04-27T11:34:17-0700">registered</milestone>
-        #     <milestone date="2010-04-29T10:12:51-0700">inprocess</milestone>
-        #     <milestone date="2010-06-15T16:08:58-0700">released</milestone>
-        #   </lifecycle>
-        def active_lifecycle(repo, druid, milestone)
-          doc = query_lifecycle(repo, druid, true)
-          milestone = doc.at_xpath("//lifecycle/milestone[text() = '#{milestone}']")
-          return Time.parse(milestone['date']) if milestone
-
-          nil
+        # @param [String] milestone_name the name of the milestone being queried for
+        # @return [Time] when the milestone was achieved.  Returns nil if the milestone does not exis
+        #
+        def active_lifecycle(repo, druid, milestone_name)
+          filter_milestone(query_lifecycle(repo, druid, true), milestone_name)
         end
 
         # @return [Hash]
@@ -57,7 +39,21 @@ module Dor
 
         private
 
+        def filter_milestone(lifecycle_doc, milestone_name)
+          milestone = lifecycle_doc.at_xpath("//lifecycle/milestone[text() = '#{milestone_name}']")
+          return unless milestone
+
+          Time.parse(milestone['date'])
+        end
+
         # @return [Nokogiri::XML::Document]
+        # @example An example lifecycle xml from the workflow service.
+        #   <lifecycle objectId="druid:ct011cv6501">
+        #     <milestone date="2010-04-27T11:34:17-0700">registered</milestone>
+        #     <milestone date="2010-04-29T10:12:51-0700">inprocess</milestone>
+        #     <milestone date="2010-06-15T16:08:58-0700">released</milestone>
+        #   </lifecycle>
+        #
         def query_lifecycle(repo, druid, active_only = false)
           req = "#{repo}/objects/#{druid}/lifecycle"
           req += '?active-only=true' if active_only
