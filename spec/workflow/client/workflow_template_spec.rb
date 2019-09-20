3e4cf9398bd6a34084289403454f2ff9fe17a31a
Justin Coyne <jcoyne@justincoyne.com>
2019-09-20 11:27:38 -0500

    Add method to retrieve a list of templates

--- spec/workflow/client/workflow_template_spec.rb
+++ spec/workflow/client/workflow_template_spec.rb
@@ -3,18 +3,28 @@
 require 'spec_helper'
 
 RSpec.describe Dor::Workflow::Client::WorkflowTemplate do
-  let(:data) { '{"processes":[{"name":"start-assembly"},{"name":"content-metadata-create"}]}' }
   let(:mock_requestor) { instance_double(Dor::Workflow::Client::Requestor, request: data) }
 
   let(:routes) { described_class.new(requestor: mock_requestor) }
 
   describe '#retrieve' do
     subject(:workflow_template) { routes.retrieve('accessionWF') }
+    let(:data) { '{"processes":[{"name":"start-assembly"},{"name":"content-metadata-create"}]}' }
 
-    it 'returns a workflow' do
+    it 'returns a workflow template' do
       expect(workflow_template['processes']).to eq [{ 'name' => 'start-assembly' },
                                                     { 'name' => 'content-metadata-create' }]
       expect(mock_requestor).to have_received(:request).with('workflow_templates/accessionWF')
+    end
+  end
+
+  describe '#all' do
+    subject(:workflow_templates) { routes.all }
+    let(:data) { '["assemblyWF","registrationWF"]' }
+
+    it 'returns a list of templates' do
+      expect(workflow_templates).to eq %w[assemblyWF registrationWF]
+      expect(mock_requestor).to have_received(:request).with('workflow_templates')
     end
   end
 end
