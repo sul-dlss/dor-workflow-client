e3a584994b7259476c15385fb62a8947855cf6b6
Justin Coyne <jcoyne@justincoyne.com>
2019-12-29 12:58:44 -0600

    Accept kwargs for close version

--- lib/dor/workflow/client/version_routes.rb
+++ lib/dor/workflow/client/version_routes.rb
@@ -17,12 +17,31 @@ module Dor
         # @param [String] repo The repository the object resides in.  The service recoginzes "dor" and "sdr" at the moment
         # @param [String] druid The id of the object to delete the workflow from
         # @param [Boolean] create_accession_wf Option to create accessionWF when closing a version.  Defaults to true
-        def close_version(repo, druid, create_accession_wf = true)
+        # rubocop:disable Metrics/MethodLength
+        def close_version(*args)
+          case args.size
+          when 3
+            Deprecation.warn(self, 'you provided 3 args, but close_version now takes kwargs')
+            (repo, druid, create_accession_wf) = args
+          when 2
+            Deprecation.warn(self, 'you provided 2 args, but close_version now takes kwargs')
+            (repo, druid) = args
+            create_accession_wf = true
+          when 1
+            opts = args.first
+            repo = opts[:repo]
+            druid = opts[:druid]
+            create_accession_wf = opts.key?(:create_accession_wf) ? opts[:create_accession_wf] : true
+          else
+            raise ArgumentError, 'wrong number of arguments, must be 1-3'
+          end
+
           uri = "#{repo}/objects/#{druid}/versionClose"
           uri += '?create-accession=false' unless create_accession_wf
           requestor.request(uri, 'post', '')
           true
         end
+        # rubocop:enable Metrics/MethodLength
 
         private
 
--- spec/workflow/client_spec.rb
+++ spec/workflow/client_spec.rb
@@ -756,13 +756,33 @@ RSpec.describe Dor::Workflow::Client do
     end
 
     let(:url) { 'dor/objects/druid:123/versionClose' }
-    it 'calls the versionClose endpoint with druid' do
-      client.close_version(@repo, @druid)
+
+    context 'with positional arguments' do
+      before do
+        allow(Deprecation).to receive(:warn)
+      end
+
+      it 'calls the versionClose endpoint with druid' do
+        client.close_version(@repo, @druid)
+        expect(Deprecation).to have_received(:warn)
+      end
+
+      it 'optionally prevents creation of accessionWF' do
+        expect(mock_http_connection).to receive(:post).with('dor/objects/druid:123/versionClose?create-accession=false').and_call_original
+        client.close_version(@repo, @druid, false)
+        expect(Deprecation).to have_received(:warn)
+      end
     end
 
-    it 'optionally prevents creation of accessionWF' do
-      expect(mock_http_connection).to receive(:post).with('dor/objects/druid:123/versionClose?create-accession=false').and_call_original
-      client.close_version(@repo, @druid, false)
+    context 'with kwargs' do
+      it 'calls the versionClose endpoint with druid' do
+        client.close_version(repo: @repo, druid: @druid)
+      end
+
+      it 'optionally prevents creation of accessionWF' do
+        expect(mock_http_connection).to receive(:post).with('dor/objects/druid:123/versionClose?create-accession=false').and_call_original
+        client.close_version(repo: @repo, druid: @druid, create_accession_wf: false)
+      end
     end
   end
 
