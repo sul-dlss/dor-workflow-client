e1a4bd3a53f2d80d8911fb6b037401c12c5938f3
Aaron Collier <aaron.collier@stanford.edu>
2020-04-02 10:21:07 -0700

    Remove repo from lifecycle routes and milestones (#157)
    * Remove repo from lifecycle routes and milestones
    
    * Switch to kwargs and deprecate positional args for lifecycle routes
    
    * We don't support 5 args
    
    * Update README

--- lib/dor/workflow/client/lifecycle_routes.rb
+++ lib/dor/workflow/client/lifecycle_routes.rb
@@ -10,37 +10,102 @@ module Dor
         end
 
         # Returns the Date for a requested milestone from workflow lifecycle
-        # @param [String] repo repository name
+        #
+        # @param [String] repo The repository the object resides in. This parameter is deprecated
         # @param [String] druid object id
         # @param [String] milestone_name the name of the milestone being queried for
         # @param [Number] version the version to query for
         # @param [Boolean] active_only (false) if true, return only lifecycle steps for versions that have all processes complete
         # @return [Time] when the milestone was achieved.  Returns nil if the milestone does not exist
         #
-        def lifecycle(repo, druid, milestone_name, version: nil, active_only: false)
-          filter_milestone(query_lifecycle(repo, druid, version: version, active_only: active_only), milestone_name)
+        # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        def lifecycle(*args)
+          case args.size
+          when 4
+            Deprecation.warn(self, 'you provided 4 args, but lifecycle now takes kwargs')
+            (repo, druid, milestone_name) = args[0..2]
+            version = args[3][:version]
+            active_only = args[3][:active_only] || false
+          when 3
+            Deprecation.warn(self, 'you provided 3 args, but lifecycle now takes kwargs')
+            (repo, druid, milestone_name) = args
+            version = nil
+            active_only = false
+          when 1
+            opts = args.first
+            repo = opts[:repo]
+            druid = opts[:druid]
+            milestone_name = opts[:milestone_name]
+            version = opts[:version]
+            active_only = opts[:active_only] || false
+          else
+            raise ArgumentError, 'wrong number of arguments, must be 1, or 3-5'
+          end
+
+          Deprecation.warn(self, 'passing the repo parameter to lifecycle is no longer necessary. This will raise an error in dor-workflow-client version 4') if repo
+
+          filter_milestone(query_lifecycle(druid, version: version, active_only: active_only), milestone_name)
         end
+        # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
         # Returns the Date for a requested milestone ONLY for the current version.
         # This is slow as the workflow server will query dor-services-app for the version.
         # A better approach is #lifecycle with the version tag.
-        # @param [String] repo repository name
         # @param [String] druid object id
         # @param [String] milestone_name the name of the milestone being queried for
         # @param [Number] version the version to query for
         # @return [Time] when the milestone was achieved.  Returns nil if the milestone does not exis
         #
-        def active_lifecycle(repo, druid, milestone_name, version: nil)
-          lifecycle(repo, druid, milestone_name, version: version, active_only: true)
+        # rubocop:disable Metrics/MethodLength
+        def active_lifecycle(*args)
+          case args.size
+          when 4
+            Deprecation.warn(self, 'you provided 4 args, but active_lifecycle now takes kwargs')
+            (repo, druid, milestone_name) = args[0..2]
+            version = args[3][:version]
+          when 3
+            Deprecation.warn(self, 'you provided 3 args, but active_lifecycle now takes kwargs')
+            (repo, druid, milestone_name) = args
+            version = nil
+          when 1
+            opts = args.first
+            repo = opts[:repo]
+            druid = opts[:druid]
+            milestone_name = opts[:milestone_name]
+            version = opts[:version]
+          else
+            raise ArgumentError, 'wrong number of arguments, must be 1, 3, or 4'
+          end
+
+          Deprecation.warn(self, 'passing the repo parameter to active_lifecycle is no longer necessary. This will raise an error in dor-workflow-client version 4') if repo
+
+          lifecycle(druid: druid, milestone_name: milestone_name, version: version, active_only: true)
         end
+        # rubocop:enable Metrics/MethodLength
 
         # @return [Array<Hash>]
-        def milestones(repo, druid)
-          doc = query_lifecycle(repo, druid, active_only: false)
+        # rubocop:disable Metrics/MethodLength
+        def milestones(*args)
+          case args.size
+          when 2
+            Deprecation.warn(self, 'you provided 2 args, but active_lifecycle now takes kwargs')
+            (repo, druid) = args
+          when 1
+            opts = args.first
+            repo = opts[:repo]
+            druid = opts.fetch(:druid)
+          else
+            raise ArgumentError, 'wrong number of arguments, must be 1-2'
+          end
+
+          Deprecation.warn(self, 'passing the repo parameter to active_lifecycle is no longer necessary. This will raise an error in dor-workflow-client version 4') if repo
+
+          doc = query_lifecycle(druid, active_only: false)
           doc.xpath('//lifecycle/milestone').collect do |node|
             { milestone: node.text, at: Time.parse(node['date']), version: node['version'] }
           end
         end
+        # rubocop:enable Metrics/MethodLength
 
         private
 
@@ -51,7 +116,6 @@ module Dor
           Time.parse(milestone['date'])
         end
 
-        # @param [String] repo repository name
         # @param [String] druid object id
         # @param [Boolean] active_only (false) if true, return only lifecycle steps for versions that have all processes complete
         # @param [Number] version the version to query for
@@ -63,8 +127,8 @@ module Dor
         #     <milestone date="2010-06-15T16:08:58-0700">released</milestone>
         #   </lifecycle>
         #
-        def query_lifecycle(repo, druid, active_only:, version: nil)
-          req = "#{repo}/objects/#{druid}/lifecycle"
+        def query_lifecycle(druid, active_only:, version: nil)
+          req = "objects/#{druid}/lifecycle"
           params = []
           params << "version=#{version}" if version
           params << 'active-only=true' if active_only
--- spec/workflow/client/lifecycle_routes_spec.rb
+++ spec/workflow/client/lifecycle_routes_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Dor::Workflow::Client::LifecycleRoutes do
   let(:requestor) { instance_double(Dor::Workflow::Client::Requestor, request: response) }
   let(:response) { '<xml />' }
   let(:routes) { described_class.new(requestor: requestor) }
+  let(:repo) { 'dor' }
+  let(:druid) { 'druid:gv054hp4128' }
 
   describe '#milestones' do
     let(:ng_xml) { Nokogiri::XML(xml) }
@@ -15,52 +17,177 @@ RSpec.describe Dor::Workflow::Client::LifecycleRoutes do
 
     before do
       allow(routes).to receive(:query_lifecycle).and_return(ng_xml)
+      allow(Deprecation).to receive(:warn)
     end
 
-    subject(:milestones) { routes.milestones('dor', 'druid:gv054hp4128') }
+    context 'with positional arguments' do
+      subject(:milestones) { routes.milestones(repo, druid) }
 
-    it 'includes the version in with the milestones' do
-      expect(milestones.first[:milestone]).to eq('published')
-      expect(milestones.first[:version]).to eq('2')
+      it 'includes the version in with the milestones' do
+        expect(milestones.first[:milestone]).to eq('published')
+        expect(milestones.first[:version]).to eq('2')
+        expect(Deprecation).to have_received(:warn).twice
+      end
+    end
+
+    context 'with kwargs' do
+      subject(:milestones) { routes.milestones(druid: druid) }
+
+      it 'includes the version in with the milestones' do
+        expect(milestones.first[:milestone]).to eq('published')
+        expect(milestones.first[:version]).to eq('2')
+      end
     end
   end
 
   describe '#lifecycle' do
-    context 'without version' do
-      subject(:lifecycle) { routes.lifecycle('dor', 'druid:gv054hp4128', 'submitted') }
+    context 'with positional arguments' do
+      before do
+        allow(Deprecation).to receive(:warn)
+      end
 
-      it 'make the request' do
-        lifecycle
-        expect(requestor).to have_received(:request).with('dor/objects/druid:gv054hp4128/lifecycle')
+      context 'without version' do
+        subject(:lifecycle) { routes.lifecycle(repo, druid, 'submitted') }
+
+        it 'make the request' do
+          lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle')
+          expect(Deprecation).to have_received(:warn).twice
+        end
+      end
+
+      context 'with version' do
+        subject(:lifecycle) { routes.lifecycle(repo, druid, 'submitted', version: 3) }
+
+        it 'makes the request with the version' do
+          lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3')
+          expect(Deprecation).to have_received(:warn).twice
+        end
       end
     end
 
-    context 'with version' do
-      subject(:lifecycle) { routes.lifecycle('dor', 'druid:gv054hp4128', 'submitted', version: 3) }
+    context 'with kwargs' do
+      before do
+        allow(Deprecation).to receive(:warn)
+      end
 
-      it 'makes the request with the version' do
-        lifecycle
-        expect(requestor).to have_received(:request).with('dor/objects/druid:gv054hp4128/lifecycle?version=3')
+      context 'with deprecated repo arg' do
+        context 'without version' do
+          subject(:lifecycle) { routes.lifecycle(repo: repo, druid: druid, milestone_name: 'submitted') }
+
+          it 'make the request' do
+            lifecycle
+            expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle')
+            expect(Deprecation).to have_received(:warn)
+          end
+        end
+
+        context 'with version' do
+          subject(:lifecycle) { routes.lifecycle(repo: repo, druid: druid, milestone_name: 'submitted', version: 3) }
+
+          it 'makes the request with the version' do
+            lifecycle
+            expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3')
+            expect(Deprecation).to have_received(:warn)
+          end
+        end
+      end
+
+      context 'without version' do
+        subject(:lifecycle) { routes.lifecycle(druid: druid, milestone_name: 'submitted') }
+
+        it 'make the request' do
+          lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle')
+          expect(Deprecation).not_to have_received(:warn)
+        end
+      end
+
+      context 'with version' do
+        subject(:lifecycle) { routes.lifecycle(druid: druid, milestone_name: 'submitted', version: 3) }
+
+        it 'makes the request with the version' do
+          lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3')
+          expect(Deprecation).not_to have_received(:warn)
+        end
       end
     end
   end
 
   describe '#active_lifecycle' do
-    context 'without version' do
-      subject(:active_lifecycle) { routes.active_lifecycle('dor', 'druid:gv054hp4128', 'submitted') }
+    context 'with positional arguments' do
+      before do
+        allow(Deprecation).to receive(:warn)
+      end
 
-      it 'make the request' do
-        active_lifecycle
-        expect(requestor).to have_received(:request).with('dor/objects/druid:gv054hp4128/lifecycle?active-only=true')
+      context 'without version' do
+        subject(:active_lifecycle) { routes.active_lifecycle(repo, druid, 'submitted') }
+
+        it 'make the request' do
+          active_lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?active-only=true')
+          expect(Deprecation).to have_received(:warn).twice
+        end
+      end
+
+      context 'with version' do
+        subject(:active_lifecycle) { routes.active_lifecycle(repo, druid, 'submitted', version: 3) }
+
+        it 'makes the request with the version' do
+          active_lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3&active-only=true')
+          expect(Deprecation).to have_received(:warn).twice
+        end
       end
     end
 
-    context 'with version' do
-      subject(:active_lifecycle) { routes.active_lifecycle('dor', 'druid:gv054hp4128', 'submitted', version: 3) }
+    context 'with kwargs' do
+      before do
+        allow(Deprecation).to receive(:warn)
+      end
 
-      it 'makes the request with the version' do
-        active_lifecycle
-        expect(requestor).to have_received(:request).with('dor/objects/druid:gv054hp4128/lifecycle?version=3&active-only=true')
+      context 'with deprecated repo arg' do
+        context 'without version' do
+          subject(:active_lifecycle) { routes.active_lifecycle(repo: repo, druid: druid, milestone_name: 'submitted') }
+
+          it 'make the request' do
+            active_lifecycle
+            expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?active-only=true')
+            expect(Deprecation).to have_received(:warn)
+          end
+        end
+
+        context 'with version' do
+          subject(:active_lifecycle) { routes.active_lifecycle(repo: repo, druid: druid, milestone_name: 'submitted', version: 3) }
+
+          it 'makes the request with the version' do
+            active_lifecycle
+            expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3&active-only=true')
+            expect(Deprecation).to have_received(:warn)
+          end
+        end
+      end
+
+      context 'without version' do
+        subject(:active_lifecycle) { routes.active_lifecycle(druid: druid, milestone_name: 'submitted') }
+
+        it 'make the request' do
+          active_lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?active-only=true')
+          expect(Deprecation).not_to have_received(:warn)
+        end
+      end
+
+      context 'with version' do
+        subject(:active_lifecycle) { routes.active_lifecycle(druid: druid, milestone_name: 'submitted', version: 3) }
+
+        it 'makes the request with the version' do
+          active_lifecycle
+          expect(requestor).to have_received(:request).with('objects/druid:gv054hp4128/lifecycle?version=3&active-only=true')
+          expect(Deprecation).not_to have_received(:warn)
+        end
       end
     end
   end
